17d65036b0b601ba974487ee4c5c13814f6ff46c
Nick930826 <xianyou1993@qq.com>
2021-05-28 12:58:52 +0800

    update

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
 const MODE = import.meta.env.MODE // 环境变量
 
-export const baseUrl = MODE == 'development' ? '/api' : 'http://api-cost.chennick.wang'
+export const baseUrl = MODE == 'development' ? '/api' : 'http://api.chennick.wang'
