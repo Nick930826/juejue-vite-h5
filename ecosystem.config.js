6d1ba1bd8ced1f9785a86fcd007fb85121424f6e
Nick930826 <xianyou1993@qq.com>
2021-07-07 17:28:56 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       ref: 'origin/master',
       repo: 'git@git.zhlh6.cn:Nick930826/juejue-vite-h5.git',
       path: '/workspace/juejue-vite-h5',
-      'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && npm run build:release && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
+      'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
       env: {
         NODE_ENV: 'production'
       }
